1a4723ac2f6aa9dc837fb551c195806530b7d5d1
Monique Lima <monique_tamara@hotmail.com>
2023-08-02 21:59:08 -0300

    first commit

---
+++ src/main/java/com/monique/workshopmongo/Workshopmongo3Application.java
@@ -0,0 +1,13 @@
+package com.monique.workshopmongo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Workshopmongo3Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Workshopmongo3Application.class, args);
+	}
+
+}
---
+++ src/test/java/com/monique/workshopmongo/Workshopmongo3ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.monique.workshopmongo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Workshopmongo3ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
